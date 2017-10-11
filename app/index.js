e81a803b23aeb63aa09585763fa34459b38bd809
aufimtsev <fimak@bk.ru>
2017-10-11 22:06:34 +0600

    telegram bot

--- app/index.js
+++ app/index.js
@@ -1,29 +1,28 @@
 'use strict'
 
 const TOKEN = '468842881:AAGI68ZCubLbUo4JF4CDVZxmvBp14eBhYz4'
+const CHAT_ID = '-218892414'
 
-const Telegram = require('telegram-node-bot')
-const TelegramBaseController = Telegram.TelegramBaseController
-const TextCommand = Telegram.TextCommand
-const tg = new Telegram.Telegram(TOKEN)
+const TelegramBot = require('node-telegram-bot-api');
 
-class PingController extends TelegramBaseController {
-    /**
-     * @param {Scope} $
-     */
-    pingHandler($) {
-        $.sendMessage('pong')
-    }
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(TOKEN, {polling: true});
 
-    get routes() {
-        return {
-            'pingCommand': 'pingHandler'
-        }
-    }
-}
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-tg.router
-    .when(
-        new TextCommand('ping', 'pingCommand'),
-        new PingController()
-    )
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/booking (.+) (.+)/, (msg, match) => {
+  const resp = `Имя: ${match[1]}\nТелефон: ${match[2]}`;
+
+  bot.sendMessage(CHAT_ID, resp);
+});
