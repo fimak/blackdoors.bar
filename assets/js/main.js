73b6125862ac810ff282b63244b039e3a81ee98c
aufimtsev <fimak@bk.ru>
2017-10-11 08:14:45 +0600

    section #four

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	skel.breakpoints({
@@ -301,10 +295,15 @@
 			$banner
 				._parallax();
 
+			var $banner2 = $('.banner');
+
+			$banner
+        ._parallax();
+
 	});
 
-	// Carousel
   $(document).ready(function(){
+    // Carousel
     $(".owl-carousel").owlCarousel({
       loop:true,
       margin:10,
@@ -323,6 +322,9 @@
       autoplayTimeout:2500,
       autoplayHoverPause:true
     });
+
+    //Mask
+    $('#email').mask('+7(000) 000-00-00');
   });
 
 })(jQuery);
